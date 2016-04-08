824ba4c52e7aeb743bd7c5b4493367d9564fa13a
jarosdavid <jarosdavid@volny.cz>
2016-04-08 12:51:26 +0200

    Úpravy nového presenteru

--- FingerFinderPresenter/MainWindow.xaml.cs
+++ FingerFinderPresenter/MainWindow.xaml.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using FingerprintAnalyzer;
+using Microsoft.Win32;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace FingerFinderPresenter
 {
@@ -28,9 +23,82 @@ namespace FingerFinderPresenter
             InitializeComponent();
         }
 
-        private void tabControl_fingerprintDrawer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void tabControl_fingerprintDrawer_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             Console.WriteLine(sender.ToString());
+            drawFingerprint();
+        }
+
+        /// <summary>
+        /// Import obrazku
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_import_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Console.WriteLine(openFileDialog.FileName);
+
+                try
+                {
+                    System.Drawing.Image fingerprint = System.Drawing.Image.FromFile(openFileDialog.FileName);
+                    this.Analyzer.FingerprintOriginal = fingerprint;
+                    this.drawFingerprint();
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
+            }
+                
+        }
+
+        private void drawFingerprint()
+        {
+            System.Drawing.Image fingerprintIm = this.Analyzer.getImage(tabControl_fingerprintDrawer.SelectedIndex);
+
+            if (fingerprintIm == null)
+            {
+                SolidColorBrush fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("HotPink"));
+                canvas.Background = fill;
+
+                return;
+            }
+
+            BitmapImage fingerprint = new BitmapImage();
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                fingerprintIm.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                fingerprint.BeginInit();
+                fingerprint.StreamSource = memory;
+                fingerprint.CacheOption = BitmapCacheOption.OnLoad;
+                fingerprint.EndInit();
+                
+            }
+
+            SolidColorBrush fillW = new SolidColorBrush((Color)ColorConverter.ConvertFromString("White"));
+            canvas.Background = fillW;
+
+            DrawingVisual drawingVisual = new DrawingVisual();
+            var draw = drawingVisual.RenderOpen();
+            draw.DrawImage(fingerprint, new Rect(0, 0, fingerprint.PixelWidth, fingerprint.PixelHeight));
+            draw.Close();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)canvas.ActualWidth, (int)canvas.ActualHeight, 120, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+
+            Image image = new Image();
+            image.Source = bmp;
+            canvas.Children.Add(image);
+        }
+
+        private void MenuItem_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
